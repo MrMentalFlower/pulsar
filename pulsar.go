917873a13085bf9eb1ca79cc849793026a51b13e
MrMentalFlower <samuelejpike@gmail.com>
2023-01-07 00:42:45 -0500

    ltr

--- pulsar.go
+++ pulsar.go
@@ -59,7 +59,7 @@ func main() {
 }
 
 // len(numbers[0]
-// need a way to save state after the new line is reached and then continue from there
+// need a way to save state after the new line is reached and then continue from ther
 func asciiPrint(input []rune) string {
 	var output string
 	var z int
